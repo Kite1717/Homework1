b6a7627922445a3e00681cabd8e11e88dfa22ca4
Kite1717 <mustafafirat.yilmaz@ogr.deu.edu.tr>
2020-01-10 23:38:37 +0300

    Create standard methods of Er.java

--- src/Soldiers/Er.java
+++ src/Soldiers/Er.java
@@ -12,14 +12,18 @@ public class Er extends Soldier {
         defineAvailableDamage();
     }
 
+    /**
+     * I added the damage the soldier can do
+     */
     private void defineAvailableDamage()
     {
         super.setDamages(new ArrayList<>(Arrays.asList(5,10,15)));
 
     }
 
-
-
+    /**
+     * Possibility of maintaining position %30
+     */
     @Override
     public void keepPosition() {
         super.keepPosition();
@@ -27,14 +31,15 @@ public class Er extends Soldier {
 
     /**
      * soldier only moves up or down except in exceptional cases
+     * possibility of movement %40
      */
     @Override
     public void move() {
 
         Random random = new Random();
-        int decision = random.nextInt() % 2;
+        int decision = random.nextInt() % 40; // normalization
 
-        if(decision == 0)// up
+        if(decision >= 0 && decision <=24 )// up %25
         {
 
             if(super.getY() != 15)
@@ -44,21 +49,33 @@ public class Er extends Soldier {
                     super.setX(super.getX() +1);
             }
         }
-        else{ //down
+        else{ //down % 15
             if(super.getY() != 0)
                 super.setY(super.getY() - 1);
             else { // corner cases
                 if(super.getX() != 0) // left
-                {
                     super.setX(super.getX() - 1);
-                }
+
             }
 
         }
     }
 
+    /**
+     * possibility of shooting % 30
+     * @return damage amount
+     */
     @Override
     public int fire() {
-        return super.fire();
+        Random random = new Random();
+
+        int decision = random.nextInt() % 30; //normalization
+
+        if(decision >= 0 && decision<=14)
+            return super.getDamages().get(0);
+        else if(decision >=15 && decision<= 24)
+            return super.getDamages().get(1);
+        else  return  super.getDamages().get(2);
+
     }
 }
