e1f8b05c6f97c04f6bde272375757450f00ad220
Kite1717 <mustafafirat.yilmaz@ogr.deu.edu.tr>
2020-01-19 14:49:49 +0300

    Override toString method

--- src/helper/Coordinate.java
+++ src/helper/Coordinate.java
@@ -23,4 +23,9 @@ public class Coordinate {
     public void setY(int y) {
         this.y = y;
     }
+
+    @Override
+    public String toString() {
+        return "X : " + this.x + "  Y : " + this.y;
+    }
 }
