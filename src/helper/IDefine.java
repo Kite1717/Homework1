24316cc7e7031d9b00c15415448d318bb6c5a4bf
Kite1717 <mustafafirat.yilmaz@ogr.deu.edu.tr>
2020-01-22 00:14:10 +0300

    Add IDefined interface

---
+++ src/helper/IDefine.java
@@ -0,0 +1,10 @@
+package helper;
+
+public interface IDefine {
+
+    /**
+     * It is used to create the amount of damage the soldier can deal according to his rank.
+     */
+    void defineAvailableDamage();
+
+}
