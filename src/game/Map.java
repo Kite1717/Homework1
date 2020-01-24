600c5cdadfd710829ff1f2806722761c94edc6a5
Kite1717 <mustafafirat.yilmaz@ogr.deu.edu.tr>
2020-01-24 16:46:29 +0300

    Add field and availablePositon method

--- src/game/Map.java
+++ src/game/Map.java
@@ -1,4 +1,60 @@
 package game;
 
+
+import helper.Coordinate;
+import soldiers.Soldier;
+
 public class Map {
+
+    private  Team t1 , t2;
+    private  int maxMapRow , maxMapColumn;
+
+    public Map(Team t1 , Team t2 , int maxMapRow,int maxMapColumn)
+    {
+        this.t1 = t1;
+        this.t2 = t2;
+        this.maxMapRow = maxMapRow;
+        this.maxMapColumn = maxMapColumn;
+
+    }
+    
+    /**
+     * Checks the coordinate suitability and checks if the soldier can perform the movement.
+     * @param coord new coordinate of soldier
+     * @return coordinate suitability
+     */
+    public boolean availablePosition(Coordinate coord)
+    {
+
+        //corner cases
+        if( !(coord.getX() >= 0 && coord.getX() <= maxMapRow &&
+            coord.getY() >= 0 && coord.getY() <= maxMapColumn))
+            return  false;
+
+        for(Soldier s : t1.getSoldiers())
+            if(s.getCoord().equals(coord))
+                return  false;
+
+        for(Soldier s : t2.getSoldiers())
+            if(s.getCoord().equals(coord))
+                return  false;
+
+            return true;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
